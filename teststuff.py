db21fef2e14b9c70e8c5be662e4db41c7b085dd8
Björn Dahlstrand <bjorndahlstrand@gmail.com>
2022-08-15 15:30:44 +0200

    wfewfwe

---
+++ teststuff.py
@@ -0,0 +1,42 @@
+text_prompts = {
+    # 0: ["Gigantic spherical alien spaceship crashed in city, overgrown by moss and vines, concept art, post apocalyptic, trending on artstation"],
+    # 0: ["Fantasy city, castle, medieval, concept art by Raphael Lacoste, digital art","artstation, deviantart"],
+    # 0: [ "A gothic vampire castle, oilpainting, trending on artstation"],
+    #  0: [ "huge wooden viking temple, concept art, trending on artstation"],
+
+    #  0: [ "space station on distant planet, alien buildings, concept art, trending on artstation"],
+     0: [ "Large rocky mountains, alien planet"],
+    #  0: [ "gigantic sea monsters, under water, trending on artstation"],
+    #  0: [ "ancient ruins, overgrown by jungle, moss, vines, volumetric light, mystic, concept art, trending on artstation"],
+    
+    
+    
+    # 0: [ "A giant bird statue on a rock in the ocean. Storm and Thunder, old harbour ruins. dramatic lighting. by Alexander Dzigurski, trending on artstation"],
+  
+    # 0: ["steampunk city, concept art"],
+    # 0: ["medieval fantasy town, huge wooden houses, fantasy world, lord of the rings,  concept art","matte painting, unreal, realistic"],
+    # 0: ["A beautiful fantasy artwork of a celestial cyberpunk castle by li shuxing, trending on artstation"],
+    # 0: ["fantasy castle and surrounding town, Storybook Illustration, Moody Lighting"],
+    # 0: ["a picture of a castle, a detailed matte painting by Raphael Lacoste"],
+    # 0: ["Fantasy castle, concept art","artstation, deviantart, Supplementary-Colors"],
+    
+}
+
+image_prompts = {
+    # 0:['ImagePromptsWorkButArentVeryGood.png:2',],
+}
+
+
+print("tjo")
+
+# print text_prompts array
+for key in text_prompts:    print(text_prompts[key])
+    
+
+# print image_prompts
+
+
+# print image_prompts array
+for key in image_prompts:    print(image_prompts[key])
+    
+print("tjo")
